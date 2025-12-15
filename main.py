c7a7036111fee519590d818a3a12a30da86fc802
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-12-15 13:25:34 -0500

    add dynamic book text retrieval

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from stats import get_word_count, get_char_count, get_count_list
+import sys
 
 def main():
-    path = "./books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")  
+        sys.exit(1)
+    
 
-    book_text = get_book_text(path)
+    book_text = get_book_text(sys.argv[1])
     
     num_words = get_word_count(book_text)
     char_count = get_char_count(book_text)
     char_list = get_count_list(char_count)
-    print_report(path, num_words, char_list)
+    print_report(sys.argv[1], num_words, char_list)
+    
 
     
 def get_book_text(file_path):
@@ -21,7 +26,7 @@ def print_report(book_path, num_words, char_list):
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    
+
     for item in char_list:
         print(f"{item["char"]}: {item["num"]}")
 
