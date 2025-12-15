70e9d5c26169b7ffe86d1866f6e2d0e1b5af34ff
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-12-15 13:29:56 -0500

    update readme

--- main.py
+++ main.py
@@ -6,15 +6,12 @@ def main():
         print("Usage: python3 main.py <path_to_book>")  
         sys.exit(1)
     
-
     book_text = get_book_text(sys.argv[1])
     
     num_words = get_word_count(book_text)
     char_count = get_char_count(book_text)
     char_list = get_count_list(char_count)
     print_report(sys.argv[1], num_words, char_list)
-    
-
     
 def get_book_text(file_path):
     with open(file_path) as file:
