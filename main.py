ff6cf18f027a882b205b3a5d3089385155e09e97
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-12-15 13:05:50 -0500

    add helper function to print report

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, get_count_list
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    path = "./books/frankenstein.txt"
+
+    book_text = get_book_text(path)
     
     num_words = get_word_count(book_text)
     char_count = get_char_count(book_text)
-
-    print(f"Found {num_words} total words")
-    print(char_count)
-
+    char_list = get_count_list(char_count)
+    print_report(path, num_words, char_list)
 
     
 def get_book_text(file_path):
     with open(file_path) as file:
         return file.read()
+    
+def print_report(book_path, num_words, char_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    
+    for item in char_list:
+        print(f"{item["char"]}: {item["num"]}")
 
+    print("============= END ===============")
 
 main()
